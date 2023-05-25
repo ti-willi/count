22de26da05fcffeb72a861e6ba28cb452e93b2d5
Willian Comiotto <willianbcomiotto@gmail.com>
2023-05-25 20:06:47 -0300

    buttons ready

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-fvac
+const btnStart = document.querySelector('[data-buttonStart');
+const btnBack = document.querySelector('[data-buttonBack]');
+const btnNext = document.querySelector('[data-buttonNext]');
+const text = document.querySelector(".text");
+
+
+var count = localStorage.getItem("key") 
+text.textContent = localStorage.getItem("key")
+
+btnStart.addEventListener("click", () => {
+    count = 0;
+    text.textContent = 0;
+})
+
+btnBack.addEventListener("click", () => {
+    if (count <= 0) {
+        count = 0;
+    }
+    else {
+        count = parseInt(count) - 1;
+    }
+    
+    text.textContent = count;
+    localStorage.setItem("key", count);
+});
+
+btnNext.addEventListener("click", () => {
+    count = parseInt(count) + 1;
+    text.textContent = count;
+    localStorage.setItem("key", count);
+});
