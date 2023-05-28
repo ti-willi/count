a2dfe1da195b88546ad6a2385a13527484ececc6
Willian Comiotto <willianbcomiotto@gmail.com>
2023-05-28 19:04:29 -0300

    refactored code

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
-const btnStart = document.querySelector('[data-buttonStart');
-const btnBack = document.querySelector('[data-buttonBack]');
-const btnNext = document.querySelector('[data-buttonNext]');
-const text = document.querySelector(".text");
+const button = document.querySelectorAll('[data-button]');
+const text = document.querySelector('[data-count]');
 
+let count = localStorage.getItem("key"); 
+text.textContent = count;
 
-var count = localStorage.getItem("key") 
-text.textContent = localStorage.getItem("key")
-
-btnStart.addEventListener("click", () => {
-    if (confirm("Tem certeza que deseja iniciar a contagem?") == true) {
-        count = 0;
-        text.textContent = 0;
-    }
-
-    
+button.forEach( (e) => {
+    e.addEventListener("click", (event) => {
+        updatedCount(event.target.dataset.button);
+        resetCount(event.target.dataset.button);
+        keyValueVerification();
+    })
 })
 
-btnBack.addEventListener("click", () => {
-    if (count <= 0) {
-        count = 0;
+function keyValueVerification() {
+    if (isNaN(count)) {
+        count = 1;
+        localStorage.setItem("key", count);
+        text.textContent = count;
+    }
+}
+
+function updatedCount(click) {
+    if (click === '-') {
+        (count <= 0) ? count = 0 : count = parseInt(count) - 1;
     }
     else {
-        count = parseInt(count) - 1;
+        count = parseInt(count) + 1; 
     }
-    
     text.textContent = count;
     localStorage.setItem("key", count);
-});
+}
 
-btnNext.addEventListener("click", () => {
-    count = parseInt(count) + 1;
-    text.textContent = count;
+function resetCount(click) {
+    if (click == 'resetCount') {
+        if (confirm("Reiniciar a contagem?") == true) {
+            count = 0;
+            text.textContent = 0;
+        }
+        else {
+            count += - 1
+            text.textContent = count;
+        }
+    }
     localStorage.setItem("key", count);
-});
+}
+
+
+
+
